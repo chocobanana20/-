96991fb538b35d17cc490252d83dbc23a8a3d2dc
hyeon <ish408501@gmail.com>
2024-01-19 16:51:13 +0900

    시험 데이터를 쓰도록

--- ch.04/test.py
+++ ch.04/test.py
@@ -1,3 +1,47 @@
+# import numpy as np
+# import sys , os
+# sys.path.append(os.pardir)
+# from dataset.mnist import load_mnist
+# from layer_net import Two_layer_net
+# import matplotlib as plt
+
+# network = Two_layer_net(input_size=784,hidden_size=100,output_size=10)
+
+# (x_train,t_train),(x_test,t_test) = load_mnist(one_hot_label = True , normalize= True)
+
+# iters_num = 10000
+# batch_size = 100
+# train_size = x_train.shape[0]
+# learning_rate = 0.1
+
+# train_loss_list = []
+
+# for _ in range(iters_num):
+#     batch_mask = np.random.choice(train_size,batch_size)
+#     x_batch = x_train[batch_size]
+#     t_batch = t_train[batch_size]
+    
+#     grad = network.numerical_grad(x_batch,t_batch)
+    
+#     for keys in ('W1','b1','W2','b2'):
+#         network.params[keys]-=learning_rate*grad[keys]
+    
+#     loss = network.loss(x_batch,t_batch)
+#     train_loss_list.append(loss)
+
+    
+    
+    
+# print(train_loss_list)
+
+# plt.plot(train_loss_list)
+# plt.xlabel('iteration')
+# plt.ylabel('loss')
+# plt.show()
+
+    
+# 훈련 데이터로 손실 함수 값을 확인하지 않고 시험 데이터로 기록
+
 import numpy as np
 import sys , os
 sys.path.append(os.pardir)
@@ -14,9 +58,13 @@ batch_size = 100
 train_size = x_train.shape[0]
 learning_rate = 0.1
 
-train_loss_list = []
+iter_per_epoch = max(1,train_size/batch_size)
 
-for _ in range(iters_num):
+train_loss_list = []
+train_acc_list=[]
+test_acc_list=[]
+
+for i in range(iters_num):
     batch_mask = np.random.choice(train_size,batch_size)
     x_batch = x_train[batch_size]
     t_batch = t_train[batch_size]
@@ -28,15 +76,19 @@ for _ in range(iters_num):
     
     loss = network.loss(x_batch,t_batch)
     train_loss_list.append(loss)
-    print(loss)
     
     
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train,t_train)
+        test_acc = network.accuracy(x_test,t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train_acc ,test_acc :"+str(train_acc)+","+str(test_acc))
     
-print(train_loss_list)
+    
+
 
 plt.plot(train_loss_list)
 plt.xlabel('iteration')
 plt.ylabel('loss')
 plt.show()
-
-    
